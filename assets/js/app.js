0fe74aab7539f2a7842cd5c1ab4276f4f1eba6dc
Ruohong Liu <ruohong.leo7@gmail.com>
2014-11-13 23:05:34 -0500

    Design with Js, only have two more functions to go

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,21 +2,42 @@
 
 $(document).ready(function(){
 	$('.csubmit').on('click', function(){
+
 		var items = $('.edititem').val();
 		var prices = $('.editprice').val();
 		var quantities = $('.editquantity').val();
-		var total =$('<li><div class="action-image"><a href="#" class="delete"><img src="assets/img/delete.png" /></a><a href="#" class="check"><img src="assets/img/check.png" /></a></div> <div class="item-data"><span class="a-item-description">' + items + '</span></div><div class="price-data"><span class="a-price"> $' + prices + '</span></div><div class="quantity-data"><span class="a-quantity">' + quantities + '</span></div></li>');
-		$('#list').prepend(total);
-		total.hide().slideDown(600);
+		var total =$('<li><div class="list-wrap"><div class="action-image"><a href="#" class="delete"><img src="assets/img/delete.png" /></a><a href="#" class="check"><img src="assets/img/check.png" /></a></div> <div class="item-data"><span class="a-item-description">' + items + '</span></div><div class="price-data"><span class="a-price"> $' + prices + '</span></div><div class="quantity-data"><span class="a-quantity">' + quantities + '</span></div></div></li>');
+		
+		if (items ==="") {
+			$('#error').fadeIn(1000);
+		}
+
+		else {
+			$('#list').prepend(total);
+			total.hide().slideDown(600);
+		};
+
+
+
+		var dprice = +prices;
+		var dquantity = +quantities;
+		var sum = dprice*dquantity;
+		var previous = parseFloat(".sum");
+		$(".sum").text(sum+previous);
 	});
 
-	$('.check').on('click', function(){
-		event.preventDefault();
-		$(this).closest('li').css("background-color", "red");
+	$('#list').on('click', '.check', function(event){
+		$(this).closest('.list-wrap').css({
+                transition: 'background-color 1s ease-in-out',
+                "background-color": "rgba(0,0,220,0.2)"
+            });
+		return false;
 	});
 
-	$('.delete').on('click', function(event){
-		event.preventDefault();
-		$(this).closest('li').remove();
+	$('#list').on('click', '.delete', function(event){
+		$(this).closest('li').fadeOut(300, function(){
+			$$(this).closest('li').remove();
+		});
+		return false;
 	});
 });
